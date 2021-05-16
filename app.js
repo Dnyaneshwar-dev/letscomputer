15f1a2b13d3b19bbb536d01d31f948187ff02205
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2021-05-16 18:12:12 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./public'))
 
-app.listen(port)
+app.listen(port,()=>{
+    console.log(`listerning at ${port}`);
+})
